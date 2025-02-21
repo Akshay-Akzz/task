5e33cdbc463da2dcc90003ca2f3c22243bbeae0d
akshay <akshay@aventusinformatics.com>
2025-02-21 17:05:00 +0530

    Task Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { lazy } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Dashboard from './Pages/Admin/Dashboard/Dash/Dashboard'
-
+import Listing from './Pages/Admin/Dashboard/List/Listing';
+import AddList from './Pages/Admin/Dashboard/List/AddList';
 
 const Layout = lazy(() => import('./Pages/Admin/Auth/Layout'));
 const Login = lazy(() => import('./Pages/Admin/Auth/Login'));
 const Register = lazy(() => import('./Pages/Admin/Auth/Register'));
 // const Dashboard = lazy(() => import('./pages/Admin/Dashboard/Dash/Dashboard'));
-const AddList = lazy(() => import('./Pages/Admin/Dashboard/List/AddList'));
-const Listing = lazy(() => import('./Pages/Admin/Dashboard/List/Listing'));
+// const AddList = lazy(() => import('./Pages/Admin/Dashboard/List/AddList'));
+// const Listing = lazy(() => import('./Pages/Admin/Dashboard/List/Listing'));
 
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem('token');
