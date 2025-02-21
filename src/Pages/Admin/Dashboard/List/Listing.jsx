c595f4da01ec059123369d79aaa23add0f14de3e
akshay <akshay@aventusinformatics.com>
2025-02-21 17:10:30 +0530

    Task Completed

--- src/Pages/Admin/Dashboard/List/Listing.jsx
+++ src/Pages/Admin/Dashboard/List/Listing.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import DeleteModal from '../../../../common/DeleteMOdal';
+import DeleteModal from '../../../../common/DeleteModal'
 
 const Listing = () => {
 
